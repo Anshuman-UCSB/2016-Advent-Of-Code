334c471d3395e2efee07e7167fc86c7c7a276d74
Anshuman-UCSB <anshuman@ucsb.edu>
2020-06-28 17:11:38 -0700

    init commit

---
+++ Day 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+int main(){
+    int loc[2] = {0,0};
+    int dir = 0;
+    fstream file("input.txt");
+    stringstream ss;
+    
+    
+}
