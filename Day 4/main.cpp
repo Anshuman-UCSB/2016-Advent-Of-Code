fa14101c3dd59fed7ccba090f6d4505c719b53a4
Anshuman-UCSB <anshuman@ucsb.edu>
2020-07-05 22:43:23 -0700

    day 4

--- Day 4/main.cpp
+++ Day 4/main.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 void part1();
+void part2();
 
 int main() {
-    part1();
+    part2();
 }
 
 struct room {
@@ -16,61 +17,66 @@ struct room {
     int id;
     string key;
     bool real;
+    string decrypt;
 
     room(string inp) {
         name = "";
         key = "";
         int ind = 0;
         while (inp[ind] < '0' || inp[ind] > '9') {
-            if(inp[ind] != '-'){
+            if (inp[ind] != '-') {
                 name += inp[ind];
             }
             ind++;
         }
         string temp = "";
-        for(int i = 0;i<3;i++){
-            temp+=inp[ind+i];
+        for (int i = 0; i < 3; i++) {
+            temp += inp[ind + i];
         }
         id = stoi(temp);
-        for(int i = inp.size()-7;i<inp.size()-2;i++){
-            key+=inp[i];
+        for (int i = inp.size() - 7; i < inp.size() - 2; i++) {
+            key += inp[i];
         }
 
         int mp[26];
-        for(int i = 0;i<26;i++){
+        for (int i = 0; i < 26; i++) {
             mp[i] = 0;
         }
-        for(auto& c: name){
-            mp[c-'a']++;
+        for (auto& c : name) {
+            mp[c - 'a']++;
         }
 
         int mx = -1;
-        for(int i =0 ;i<26;i++){
+        for (int i = 0; i < 26; i++) {
             mx = max(mx, mp[i]);
             // cout<<char(i+'a')<<": "<<mp[i]<<endl;
         }
         temp = "";
         char cTemp;
-        while(temp.size()<5){
-            for(int i = 0; i<26;i++){
-                if(mp[i] == mx){
-                    cTemp = i+'a';
-                    temp+= cTemp;
+        while (temp.size() < 5) {
+            for (int i = 0; i < 26; i++) {
+                if (mp[i] == mx) {
+                    cTemp = i + 'a';
+                    temp += cTemp;
                 }
             }
             mx--;
         }
 
-        temp = temp.substr(0,5);
-        if (temp == key){
-            real = true;
-        }else{
-            real = false;
-        }
+        temp = temp.substr(0, 5);
+        real = (temp == key);
         // cout<<name<<endl;
         // cout<<id<<endl;
         // cout<<key<<endl;
         // cout<<temp<<endl<<endl;
+        int t;
+        for (auto& c : name) {
+            t = c - 'a';
+            t += id;
+            t %= 26;
+            t += 'a';
+            decrypt += char(t);
+        }
     }
 };
 
@@ -82,10 +88,25 @@ void part1() {
         v.push_back(room(line));
     }
     int sum = 0;
-    for(auto& room: v){
-        if(room.real){
-            sum+=room.id;
+    for (auto& room : v) {
+        if (room.real) {
+            sum += room.id;
         }
     }
-    cout<<sum<<endl;
+    cout << sum << endl;
+}
+
+void part2() {
+    vector<room> v;
+    fstream file("input");
+    string line;
+    while (getline(file, line)) {
+        v.push_back(room(line));
+    }
+
+    for (room& r : v) {
+        if (r.real && r.decrypt.find("north") != string::npos) {
+            cout << r.decrypt << " - " << r.id << endl;
+        }
+    }
 }
