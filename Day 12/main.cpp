a075e7235c9635210b418a922716b0650010590b
Anshuman-UCSB <anshuman@ucsb.edu>
2020-09-04 15:39:12 -0700

    No issues.

--- Day 12/main.cpp
+++ Day 12/main.cpp
@@ -1,68 +1,60 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <vector>
 #include <sstream>
 
 using namespace std;
 
-vector<string> read(){
+int main(){
     fstream file("input");
     string line;
-    vector<string> out;
+    vector<int> regs = {0, 0, 1, 0};
+    vector<string> raw;
     while(getline(file,line)){
-        out.push_back(line);
+        raw.push_back(line);
     }
-    return out;
-}
 
-int main(){
-    int regs[4] = {0, 0, 0, 0};
-    vector<string> raw = read();
-
-    for(int i = 0;i<raw.size();i++){
+    for(int i = 0;i<raw.size();){
         stringstream ss(raw[i]);
-        int val;
-        string temp, a, b;
-        ss>>temp;
-        switch(temp[0]){
-            case 'i':
-                ss>>a;
-                regs[a[0]-'a']++;
-                // cout<<"Incrementing "<<a[0]<<endl;
-                break;
-            case 'd':
-                ss>>a;
-                regs[a[0]-'a']--;
-                // cout<<"Decrementing "<<a[0]<<endl;
-                break;
-            case 'c':
-                ss>>a>>b;
-                if(a[0]>='a' && a[0]<='d'){
-                    val = regs[a[0]-'a'];
-                }else{
-                    val = stoi(a);
-                }
-                // cout<<"Copying "<<val<<" to "<<b[0]<<endl;
-                regs[b[0]-'a'] = val;
-                break;
-            case 'j':
-                ss>>a>>b;
-                if(a[0]>='a' || a[0]<='d'){
-                    val = regs[a[0]-'a'];
-                }else{
-                    val = stoi(a);
-                }
-                if(val){
-                    // cout<<"Jumping "<<stoi(b)<<endl;
-                    i+=stoi(b);
-                }else{
-                    // cout<<"Not jumping"<<endl;
-                }
-
-                break;
+        int temp;
+        // cout<<ss.str()<<endl;
+        ss>>line;
+        if(line=="cpy"){
+            ss>>line;
+            try{
+                temp = stoi(line);
+            }catch(...){
+                temp = regs[line[0]-'a'];
+            }
+            ss>>line;
+            regs[line[0]-'a'] = temp;
+                i++;
+        }else if(line == "inc"){
+            ss>>line;
+            regs[line[0]-'a']++;
+            i++;            
+        }else if(line == "dec"){
+            ss>>line;
+            regs[line[0]-'a']--;
+            i++;
+        }else{
+            ss>>line;
+            try{
+                temp = stoi(line);
+            }catch(...){
+                temp = regs[line[0]-'a'];
+            }
+            if(temp){
+                ss>>line;
+                i+=stoi(line);
+            }else{
+                i++;
+            }
         }
+            
     }
-    for(auto& r: regs){
-        cout<<r<<" ";
-    }cout<<endl;
+
+    for(int i = 0;i<4;i++){
+        cout<<char('a'+i)<<": "<<regs[i]<<endl;
+    }
 }
